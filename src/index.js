ab217c7aae09449452f6c79b176c862d38a14860
juds-create <judsonraphael73@gmail.com>
2025-03-08 15:20:07 -0300

    testando  o projeto

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
-let nomeHeroi = "juds-espadachim"
-let quantidadeDeXP = 1000
+const nomeHeroi = "juds-espadachim"
+const quantidadeDeXP = "1000"
+let nivel;
 
+if(quantidadeDeXP < 1000){
+    nivel = "Ferro"
+} else if(quantidadeDeXP <=2000){
+    nivel = "Bronze"
+} else{
+    nivel = "evoluindo"
+}
 
-
-
+console.log("o heroi de nome " + nomeHeroi + " esta no nivel " + nivel);
